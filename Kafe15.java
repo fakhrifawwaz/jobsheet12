d235bdc51fb6b1e9f6a24c1bb50baf52976cc810
fakhrifawwaz <email@fakhri07fawwaz@gmai.com>
2025-11-26 10:41:20 +0700

    Modifikasi 1

--- Kafe15.java
+++ Kafe15.java
@@ -1,8 +1,15 @@
 public class Kafe15{
-    public static void Menu(String namaPelanggan, boolean isMember) {
+    public static void Menu(String namaPelanggan, boolean isMember, String kodePromo) {
         System.out.println("Selamat datang, " + namaPelanggan + "!");
         if (isMember) {
             System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap pembelian!");
+        }
+        if (kodePromo.equals("DISKON50")) {
+            System.out.println("Selamat anda mendapatkan diskon sebesar 50%");
+        }else if (kodePromo.equals("DISKON30")){
+            System.out.println("Selamat anda mendapatkan diskon sebesar 30%");
+        }else if (!kodePromo.isEmpty()){
+            System.out.println("Kode promo invalid");
         }
 
         System.out.println("===== MENU RESTO KAFE =====");
@@ -17,6 +24,6 @@ public class Kafe15{
     }
 
     public static void main(String[] args) {
-        Menu("Andi",true);
+        Menu("Budi",true, "DISKON30");
     }
 }
